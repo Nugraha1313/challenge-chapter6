17072f76d71caccf5f3cc93952be2e19e1ce4f3c
Nugraha <aulia.nugraha13@gmail.com>
2023-05-23 22:50:27 +0700

    redefine models

--- seeders/20230521114439-component_suppliers.js
+++ seeders/20230521114439-component_suppliers.js
@@ -4,35 +4,34 @@
 module.exports = {
   async up (queryInterface, Sequelize) {
     await queryInterface.bulkInsert(
-      "component_suppliers",
+      "ProductComponents",
       [
         {
           component_id: 1,
-          supplier_id: 1
+          supplier_id: 1,
         },
         {
           component_id: 2,
-          supplier_id: 2
+          supplier_id: 2,
         },
         {
           component_id: 3,
-          supplier_id: 3
+          supplier_id: 3,
         },
         {
           component_id: 4,
-          supplier_id: 4
+          supplier_id: 4,
         },
         {
           component_id: 5,
-          supplier_id: 5
+          supplier_id: 5,
         },
-       
       ],
       {}
     );
   },
 
   async down (queryInterface, Sequelize) {
-      await queryInterface.bulkDelete('component_suppliers', null, {});
+      await queryInterface.bulkDelete("ProductComponents", null, {});
   }
 };
